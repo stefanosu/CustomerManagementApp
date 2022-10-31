b81a241424747abf21bdf60fb6d5f60dddd5824c
Stefanos <stefanosu422@gmail.com>
2022-10-31 18:40:20 -0400

    inital commit

---
+++ Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManangement
+{
+  public class Customer
+  {
+    public string FirstName { get; set; }
+    private string _lastName;
+    public string LastName
+    {
+      get
+      {
+        return _lastName;
+      }
+      set
+      {
+        _lastName = value;
+      }
+    }
+  }
+}
